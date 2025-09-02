cdcba0b4ac027d27de1d04ccaef7404dc0e54107
hasbihasbullh <mhasbihasbullah@gmail.com>
2025-09-02 21:42:54 +0800

    dev : update footer

--- common/components/layouts/Footer.tsx
+++ common/components/layouts/Footer.tsx
@@ -5,12 +5,12 @@ export const Footer = () => {
   return (
     <footer className="bg-zinc-200 dark:bg-zinc-900 shadow border-t border-border p-4">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+        <div className="flex flex-col items-center sm:flex-row sm:items-center sm:justify-between gap-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3">
             {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
             <span className="text-2xl font-semibold text-foreground">Focusity</span>
           </a>
-          <ul className="flex flex-wrap gap-4 text-sm font-medium text-muted-foreground">
+          <ul className="flex flex-wrap justify-center gap-4 text-sm font-medium text-muted-foreground">
             <li>
               <a href="#" className="hover:text-foreground transition-colors">
                 Privacy Policy
