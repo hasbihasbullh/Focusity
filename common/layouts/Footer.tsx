f74125c5c65bf6de6f1c73f8ec4b78776acd6ec7
hasbihasbullh <mhasbihasbullah@gmail.com>
2025-09-04 14:46:40 +0800

    dev : add privacy-policy & terms-of-service

--- common/layouts/Footer.tsx
+++ common/layouts/Footer.tsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { Separator } from "@/common/components/ui";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className="bg-zinc-200 dark:bg-zinc-900 shadow border-t border-border p-4">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center sm:flex-row sm:items-center sm:justify-between gap-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3">
-            {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+          <Link href="/" className="flex items-center space-x-3">
             <span className="text-2xl font-semibold text-foreground">Focusity</span>
-          </a>
+          </Link>
           <ul className="flex flex-wrap justify-center gap-4 text-sm font-medium text-muted-foreground">
             <li>
-              <a href="#" className="hover:text-foreground transition-colors">
+              <Link href="/privacy-policy" className="hover:text-foreground transition-colors">
                 Privacy Policy
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:text-foreground transition-colors">
+              <Link href="/terms-of-service" className="hover:text-foreground transition-colors">
                 Terms of Service
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
         <Separator className="my-6" />
         <div className="text-center text-sm text-muted-foreground">
           © {new Date().getFullYear()}{" "}
-          <a href="https://hasbihasbullh.vercel.app/" className="hover:text-foreground transition-colors" target="_blank">
+          <Link href="https://hasbihasbullh.vercel.app/" className="hover:text-foreground transition-colors" target="_blank">
             M Hasbi Hasbullah
-          </a>
+          </Link>
           . All Rights Reserved.
         </div>
       </div>
